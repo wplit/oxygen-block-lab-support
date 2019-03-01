52c599e290be5b5abb4f119e338ec0817c0a322c
wplit <heywplit@gmail.com>
2019-03-01 12:53:45 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Oxygen Block Lab Support #
+**Contributors:** David Browne  
+**Tags:** edd, oxygen  
+**License:** GPLv2 or later  
+**License URI:** http://www.gnu.org/licenses/gpl-2.0.html
+
+[Block lab](https://wordpress.org/plugins/block-lab/) is a free plugin that offers an easy way to add custom Gutenberg blocks to WordPress. By default, a theme is needed to store the template files for each block. This plugin simply allows you to add your new block templates without the need for a theme. Built especially for [Oxygen](http://oxygenbuilder.com/) where the theme is completely disabled.
+
+See [documentation on customising template files in Easy Digital Downloads](https://docs.easydigitaldownloads.com/article/1216-moving-edd-templates-to-your-theme) for reference.
+ 
+
+## Installation ##
+
+1. [Download the plugin here.](https://github.com/wplit/oxygen-block-lab-support/archive/master.zip)
+2. Go to Plugins > Add New in your WordPress admin. Click on `Upload Plugin` and browse for the zip file.
+3. Activate the plugin.
+
+## Usage ##
+
+1. Add your block template files for your custom blocks into block_lab_templates/blocks/ found in this plugin.
+2. In the edit block screen, the template metabox should be green and say 'Template found'.
+
+## Changelog ##
+
+### 1.0.0 - Mar 1, 2019 ###
+* Initial Release
---
+++ oxygen-block-lab-support.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Plugin Name: Oxygen Blocklab Support
+ * Plugin URI:  https://github.com/wplit/oxygen-block-lab-support/
+ * GitHub URI:  wplit/oxygen-edd-support/
+ * Description: Allows Block Lab block template customisation without using a theme. Made for Oxygen Builder.
+ * Version:     1.0.0
+ * Author:      David Browne
+ * Author URI:  https://wplit.com/
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License version 2, as published by the
+ * Free Software Foundation.  You may NOT assume that you can use any other
+ * version of the GPL.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.
+ *
+ * @package    OxygenBlockLabSupport
+ * @since      1.0.0
+ * @copyright  Copyright (c) 2018, David Browne
+ * @license    GPL-2.0+
+ */
+
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+
+add_filter( 'block_lab_template_path', 'lit_new_block_template_path' );
+/**
+ * Adds plugin subdirectory as new location for block lab to look for block templates.
+ *
+ * @since 1.0.0
+ *
+ * @param $template_path 
+ * @return new template location as /block_lab_templates/
+ */
+function lit_new_block_template_path($template_path) {
+    
+	$template_path = plugin_dir_path( __FILE__ ) . 'block_lab_templates';
+    
+	return $template_path;
+}
